d98677a60a921df70a235bc51392bd39a70e3d14
Corey Werner <cnotethegr8@gmail.com>
2019-03-13 16:30:55 +0200

    updated header links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,7 +38,9 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { search: false },
+    { href: 'https://www.kin.org/developers', label: "Developers" },
+    { href: 'https://www.kin.org/developers/features', label: "Build" },
+    { href: 'https://www.kin.org/developers/downloads', label: "Downloads" },
   ],
 
   // If you have users set above, you add it here:
