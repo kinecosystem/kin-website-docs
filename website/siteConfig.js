a16a5966a836eb535ac8cfa513b80f5e8d47c16a
Corey Werner <cnotethegr8@gmail.com>
2019-08-07 10:39:26 +0300

    removed css warning

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -91,14 +91,14 @@ const siteConfig = {
 
 
   // Add custom scripts here that would be placed in <script> tags.
-  //scripts: ['https://buttons.github.io/buttons.js'],
   scripts: [
     'https://buttons.github.io/buttons.js',
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
     '/js/code-block-buttons.js',
   ],
 
-  stylesheets: ['/css/code-block-buttons.css'],
+  // Styles from /website/static/css are automatically included.
+  stylesheets: [],
 
   gaTrackingId: 'UA-136101659-1',
 
