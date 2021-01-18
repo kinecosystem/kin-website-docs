12e7e2203146c0f81df377155a4dd9464d0fd5fd
kikengineering <57953608+kikengineering@users.noreply.github.com>
2021-01-18 18:33:51 -0500

    website: update header links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,8 +38,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { href: 'https://www.kin.org/developers', label: "Developers" },
-    { href: 'https://www.kin.org/developers/features', label: "Build" },
-    { href: 'https://www.kin.org/developers/downloads', label: "Downloads" },
+    { href: 'https://github.com/kinecosystem', label: "Build" },
   ],
 
   // If you have users set above, you add it here:
