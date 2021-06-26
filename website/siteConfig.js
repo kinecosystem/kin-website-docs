cb52a830441071b5200b71a23eccfcd694e792fd
Bram Borggreve <borggreve@gmail.com>
2021-06-25 20:37:25 -0500

    feat: add Tutorials link to header

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -37,8 +37,9 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { href: 'https://www.kin.org/developers', label: "Developers" },
-    { href: 'https://github.com/kinecosystem', label: "Build" },
+    { href: 'https://www.kin.org/tutorials', label: 'Tutorials' },
+    { href: 'https://www.kin.org/developers', label: 'Developers' },
+    { href: 'https://github.com/kinecosystem', label: 'Build' },
   ],
 
   // If you have users set above, you add it here:
